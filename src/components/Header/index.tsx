9ff020da3f0c9cb4caeb6c7176ef1ec525223e38
Gustavo Pessoa <gustavo.pessoa@gmail.com>
2021-03-23 11:02:04 +0000

    202103231101

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,36 @@
+import { useState } from 'react';
+
+import Modal from 'react-modal';
 import logoImg from '../../assets/logo.svg'
+
 import { Container, Content } from './styles';
 
 export function Header() {
+    const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+    function handleOpenNewTransactionModal() {
+        setIsNewTransactionModalOpen(true);
+    }
+
+    function handleCloseNewTransactionModal() {
+        setIsNewTransactionModalOpen(false);
+    }
+
     return (
         <Container>
             <Content>
                 <img src={logoImg} alt="Cashflow" />
-                <button type="button">
+                <button type="button" onClick={handleOpenNewTransactionModal}>
                     New transaction
                 </button>
-                </Content>
+                <Modal 
+                    isOpen={isNewTransactionModalOpen} 
+                    onRequestClose={handleCloseNewTransactionModal}
+                >
+                    <h2>Cadastrar transação</h2>
+                </Modal>
+
+            </Content>
         </Container>
     );
 }
